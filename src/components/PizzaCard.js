817ef13c84fa855928629e106c46074730ad2cda
Oluwaseyi Adeogun <sheyilaaw98@gmail.com>
2020-05-07 17:10:12 +0100

    Show add to cart button on menu list

--- src/components/PizzaCard.js
+++ src/components/PizzaCard.js
@@ -1,15 +1,22 @@
 import React from "react";
-import {Badge, Card, CardBody, CardImg, CardTitle} from "shards-react";
+import {Badge, Button, Card, CardBody, CardFooter, CardHeader, CardImg} from "shards-react";
 
 export const PizzaCard = ({ name, description, price }) => {
     return (
         <Card className="box" style={{maxWidth: "300px"}}>
+            <CardHeader>
+                {name}
+            </CardHeader>
             <CardImg src="https://place-hold.it/300x200"/>
             <CardBody>
-                <CardTitle>{name}</CardTitle>
                 <p>{description}</p>
                 <Badge>${ price }</Badge>
             </CardBody>
+            <CardFooter>
+                <Button outline pill theme="dark">
+                    Add to Cart
+                </Button>
+            </CardFooter>
         </Card>
     );
 };
