431c4c7638793f678ef9e3a6f5175ac9f4e5ea0e
Oluwaseyi Adeogun <sheyilaaw98@gmail.com>
2020-05-08 11:26:04 +0100

    Enable state persist using redux persist

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import store from "./redux/store";
+import {persistor, store} from './redux/store';
 import {Provider} from "react-redux";
+import {PersistGate} from 'redux-persist/integration/react';
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>,
   document.getElementById('root')
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,16 @@
-import {applyMiddleware, createStore} from 'redux'
-import rootReducer from './rootReducer'
+import {applyMiddleware, createStore} from 'redux';
+import {persistReducer, persistStore} from 'redux-persist';
 import thunk from "redux-thunk";
+import storage from 'redux-persist/lib/storage';
 
-const store = createStore(
-    rootReducer,
-    applyMiddleware(thunk)
-);
+import rootReducer from './rootReducer';
 
-export default store;
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer, applyMiddleware(thunk));
+export const persistor = persistStore(store);
