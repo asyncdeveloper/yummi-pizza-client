5dfd503fcfef98f8fd6651cdcb51ae8595393b13
Oluwaseyi Adeogun <sheyilaaw98@gmail.com>
2020-05-09 14:59:10 +0100

    set api base url

--- src/config/API.js
+++ src/config/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: process.env.API_URL || 'http://127.0.0.1:8000/' ,
+    baseURL: 'https://yummi-pizza-server.herokuapp.com/',
     responseType: "json",
     timeout: 5000
 });
